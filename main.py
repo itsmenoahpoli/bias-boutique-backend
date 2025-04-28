dc38dabfc26e6c435a19d0aaad6604f44802d392
Patrick Policarpio <patrickpolicarpio08@gmail.com>
2025-04-28 16:56:40 +0800

    fix

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ app = FastAPI(
 # CORS middleware configuration
 app.add_middleware(
 	CORSMiddleware,
-	allow_origins=["http://localhost:5173", "https://biasboutiquedashboard.up.railway.app"],
+	allow_origins=["*"],
 	allow_credentials=True,
 	allow_methods=["*"],
 	allow_headers=["*"],
